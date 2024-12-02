44163b45063c851144d29a4ba2315637e461b92a
Jan Klimo <jan.klimo@gmail.com>
2024-12-02 12:26:05 +0700

    Fix chart value

--- src/components/ChartInner.tsx
+++ src/components/ChartInner.tsx
@@ -10,12 +10,14 @@ interface Props {
 const ChartInner: FC<Props> = ({ tokenInfo }) => {
   if (!tokenInfo) return;
 
+  const totalSupply = parseFloat(tokenInfo.totalSupply);
+
   return (
     <div className='absolute top-1/2 left-1/2 text-white -translate-x-1/2 -translate-y-1/2 -mt-9 md:-mt-10'>
       <span className='font-bold text-sm text-hlGray'>Total Supply</span>
       <div className='text-accent font-mono font-semibold text-lg md:text-2xl mt-1'>
         <NumberFlow
-          value={parseFloat(tokenInfo.circulatingSupply)}
+          value={totalSupply}
           format={{
             minimumFractionDigits: 2,
             maximumFractionDigits: 2,
