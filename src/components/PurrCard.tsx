1ac1d6260b49df96617fbf173b8e1faefd8fcea3
Jan Klimo <jan.klimo@gmail.com>
2024-12-02 10:03:42 +0700

    HYPE card

--- src/components/PurrCard.tsx
+++ src/components/PurrCard.tsx
@@ -8,14 +8,14 @@ interface Props {
 const PurrCard: FC<Props> = ({ price }) => {
   return (
     <a
-      href='https://www.coingecko.com/en/coins/purr-2'
+      href='https://www.coingecko.com/en/coins/hyperliquid'
       target='_blank'
       rel='noopener noreferrer'
       className='inline-flex items-center cursor-newtab bg-hl-light py-1 px-2 rounded-2xl mr-2 my-1 md:my-0 hover:bg-primary-900 duration-200 transition-colors'
     >
       <div className='relative w-6 inline-block mr-1'>
         <Image
-          src='https://coin-images.coingecko.com/coins/images/37125/large/PURR_CG.png?1713368828'
+          src='https://coin-images.coingecko.com/coins/images/50882/large/hyperliquid.jpg?1729431300'
           width={64}
           height={64}
           priority
@@ -23,7 +23,7 @@ const PurrCard: FC<Props> = ({ price }) => {
           alt='PURR'
         />
       </div>
-      <p className='text-hlGray mr-2 font-normal'>PURR:</p>
+      <p className='text-hlGray mr-2 font-normal'>HYPE:</p>
       <p className='text-accent font-bold'>
         {price.toLocaleString('en-US', {
           style: 'currency',
