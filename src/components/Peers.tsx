909921a47434af2c735fe976879601cf194ff996
Jan Klimo <jan.klimo@gmail.com>
2024-12-13 20:31:50 +0700

    Select market cap or FDV sort

--- src/components/Peers.tsx
+++ src/components/Peers.tsx
@@ -12,6 +12,7 @@ import { pointToHypeRatio } from '@/constant/constants';
 import { PeersData } from '@/types/responses';
 
 type Sort = 'market_cap' | 'fdv';
+
 const processCoinData = (
   coins: PeersData,
   markPrice: number,
@@ -20,10 +21,11 @@ const processCoinData = (
   sort: Sort,
 ) => {
   const hypeFdv = markPrice * totalSupply;
-
+  const hypeMarketCap = markPrice * circulatingSupply;
   const allCoins = [
     {
       symbol: '$800/pt',
+      // markPrice * 5.35 = 800
       fdv: (800 / pointToHypeRatio) * totalSupply,
       market_cap: (800 / pointToHypeRatio) * circulatingSupply,
       url: 'https://x.com/crypto_adair/status/1806748433593577833',
@@ -37,8 +39,12 @@ const processCoinData = (
     .map((coin) => ({
       key: coin.symbol,
       symbol: coin.symbol,
-      price: coin.fdv / totalSupply,
-      multiple: coin.fdv / hypeFdv,
+      price:
+        sort === 'fdv'
+          ? coin.fdv / totalSupply
+          : coin.market_cap / circulatingSupply,
+      multiple:
+        sort === 'fdv' ? coin.fdv / hypeFdv : coin.market_cap / hypeMarketCap,
       url: coin.url,
       image_url: coin.image_url,
     }));
@@ -51,7 +57,7 @@ interface Props {
 
 const Peers: FC<Props> = ({ data, tokenInfo }) => {
   const [coins, setCoins] = useState<PeersData>([]);
-  const [sort, setSort] = useState<Sort>('fdv');
+  const [sort, setSort] = useState<Sort>('market_cap');
 
   useEffect(() => {
     fetch(`${apiHost}/peers?coin=hype`)
@@ -74,7 +80,20 @@ const Peers: FC<Props> = ({ data, tokenInfo }) => {
       <h2 className='md:flex items-center justify-center text-white text-base mb-6'>
         <span className='block md:inline mr-2'>Price of</span>
         <HypeCard price={markPrice} />
-        <span className='block md:inline'>with the FDV of ...</span>
+        <div className='block md:flex items-center'>
+          <span>with the</span>
+          <select
+            id='sort'
+            name='sort'
+            className='mx-2 inline-block bg-white/5 rounded-md border border-hl-light py-1.5 pl-3 pr-10 text-gray-300 ring-0 focus:ring-1 focus:border-hl-primary focus:ring-hl-primary sm:text-sm sm:leading-6'
+            value={sort}
+            onChange={(event) => setSort(event.target.value as Sort)}
+          >
+            <option value='market_cap'>Market cap</option>
+            <option value='fdv'>FDV</option>
+          </select>
+          <span>of ...</span>
+        </div>
       </h2>
       <section className='flex justify-center items-center flex-wrap'>
         {processCoinData(
