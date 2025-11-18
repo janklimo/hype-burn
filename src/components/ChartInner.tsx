83f1e8b58144a740c99e8c6614744afa090f4d3a
Jan Klimo <jan.klimo@gmail.com>
2025-11-18 22:06:06 +0700

    Shift chart inner

--- src/components/ChartInner.tsx
+++ src/components/ChartInner.tsx
@@ -15,7 +15,7 @@ const ChartInner: FC<Props> = ({ tokenInfo, burntEVMBalance }) => {
   const adjustedSupply = totalSupply - burntEVMBalance;
 
   return (
-    <div className='absolute text-center top-1/2 left-1/2 text-white -translate-x-1/2 -translate-y-1/2 -mt-24 md:-mt-10'>
+    <div className='absolute text-center top-1/2 left-1/2 text-white -translate-x-1/2 -translate-y-1/2 -mt-28 md:-mt-10'>
       <span className='font-bold text-sm text-hlGray'>Total Supply</span>
       <div className='text-accent font-mono font-semibold text-lg md:text-2xl mt-1'>
         <NumberFlow
